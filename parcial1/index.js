6ff2b58a8b626cd92e7bb62459dd56d833e38c42
Marvin31P <109772479+Marvin31P@users.noreply.github.com>
2024-03-13 15:22:12 -0600

    Delete parcial1 directory

--- parcial1/index.js
+++
@@ -1,8 +0,0 @@
-import {createRequire} from "module"
-const require = createRequire(import.meta.url)
-const mysql = require("./node_modules/mysql")
-
-const cotnetion = mysql.createConnection({
-    host: "localHost"
-    
-})
